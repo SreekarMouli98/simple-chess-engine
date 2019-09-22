f0efcd58a690d4673cccba1e131582b18f32232e
mouli2105 <sreekar.mouli1998@gmail.com>
2019-09-22 19:14:35 +0530

    Fixed map.js

--- map.js
+++ map.js
@@ -2,11 +2,11 @@ class Map {
     getNumericPosition(pos) {
         return [
             7 - parseInt(pos[1]) + 1,
-            'abcdefg'.indexOf(pos[0].toLowerCase())
+            'abcdefgh'.indexOf(pos[0].toLowerCase())
         ]
     }
     getAlphaPosition(pos) {
-        return 'ABCDEFG'[pos[1]]+ (8 - pos[0]).toString()
+        return 'ABCDEFGH'[pos[1]]+ (8 - pos[0]).toString()
     }
 }
 
